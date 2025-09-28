a82149b87b1d34a37127e1fba7b4e5c20a875a47
kRamu81 <kanamramu18@gmail.com>
2025-09-28 08:09:46 +0530

    update

--- Day1/Leetcode204.java
+++ Day1/Leetcode204.java
@@ -7,21 +7,48 @@ public class Leetcode204 {
         System.out.println(countPrimes(n));
     }
 
+    // public static int countPrimes(int n) {
+    //     int count = 0;
+    //     for (int i = 2; i < n; i++) {   
+    //         if (isPrime(i)) {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // public static boolean isPrime(int num) {
+    //     if (num < 2) return false;
+    //     for (int i = 2; i * i <= num; i++) {  // checking from sqrt 
+    //         if (num % i == 0) return false;
+    //     }
+    //     return true;
+    // }
+
+    
     public static int countPrimes(int n) {
-        int count = 0;
-        for (int i = 2; i < n; i++) {   
-            if (isPrime(i)) {
+        if(n<=2) return 010;
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime,true);
+        //[true,true,true,true,true,true,true,true,true,true]
+        prime[0] = false;
+        prime[1] = false;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<n;j+=i){//4,10,2 ->6,10,3  ->step replace false
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(prime[i]){
                 count++;
             }
         }
         return count;
+
     }
 
-    public static boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i * i <= num; i++) {  // checking from sqrt 
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
 }
