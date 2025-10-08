045e89cb1b6ddb07e98f0ae9b484de2f1381edda
kRamu81 <kanamramu18@gmail.com>
2025-10-08 21:20:53 +0530

    day 11

---
+++ Day11/PermutationinString567.java
@@ -0,0 +1,14 @@
+package Day11;
+import java.util.*;
+
+public class PermutationinString567 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.next();
+        String s2 = s.next();
+        System.out.println(permutationofString(s1,s2));
+    }
+    public static boolean permutationofString(String s1, String s2){
+
+    }
+}
