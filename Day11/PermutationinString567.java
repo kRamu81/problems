24d0e5c24a53b2b35ea408bed434ff92673a4f76
kRamu81 <kanamramu18@gmail.com>
2025-10-08 21:48:18 +0530

    day 11 update

--- Day11/PermutationinString567.java
+++ Day11/PermutationinString567.java
@@ -1,14 +1,31 @@
 package Day11;
 import java.util.*;
+import java.util.Arrays;
 
 public class PermutationinString567 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String s1 = s.next();
-        String s2 = s.next();
+        String s1 = s.next();//ab
+        String s2 = s.next();//eidbaooo
         System.out.println(permutationofString(s1,s2));
     }
     public static boolean permutationofString(String s1, String s2){
+        if(s1.length()>s2.length()) return false;
+        int[] freq = new int[26];
+        int[] window = new int[26];
+        for(int i=0;i<s1.length();i++){// count the freq of the character
+            freq[s1.charAt(i)-'a']++;
+            window[s2.charAt(i)-'a']++;
+        }
+        //using sliding window
+        for(int i=s1.length();i<s2.length();i++){
+            // if both are equal directly return true
+            if(Arrays.equals(freq,window)) return true;
+            //not same update right add and left remove
+            window[s2.charAt(i)-'a']++;// adding right side update
+            window[s2.charAt(i-s1.length())-'a']--;//remove the left character
+        }
+        return Arrays.equals(freq,window);
 
     }
 }
