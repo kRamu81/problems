e047ebb68b2ff0f340df8c48db73f816dd63872d
kRamu81 <kanamramu18@gmail.com>
2025-10-13 08:47:11 +0530

    update stack 2

--- Stack/GenerateParenthsis22.java
+++ Stack/GenerateParenthsis22.java
@@ -1,4 +1,35 @@
 package Stack;
+import java.util.*;
 
 public class GenerateParenthsis22 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(generateP(n));
+    }
+
+    public static List<String> generateP(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n); // fixed StringBuilder + n
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1); // fixed deleteCharAt
+        }
+
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
 }
--- Stack/MinStack155.java
+++ Stack/MinStack155.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class MinStack155 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 //        ["MinStack","push","push","push","getMin","pop","top","getMin"]
 //        [[],[-2],[0],[-3],[],[],[],[]]
-
-
-
+        String[] words = s.nextLine().trim().split("\\s+");
+        List<List<Integer>> num = s.nextLine().trim().split("\\s+");
     }
 }
